3aad251c81c664715b660f369cee3cafc92f0a8b
Alexey Kinyov <rudy@05bit.com>
2015-07-08 01:06:07 +0300

    Initial commit

---
+++ ZeroFive.Forms.Images.iOS/RoundedImageRenderer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using Foundation;
+using ZeroFive.Forms.Images;
+using ZeroFive.Forms.Images.iOS;
+
+
+[assembly: ExportRenderer(typeof(RoundedImage), typeof(RoundedImageRenderer))]
+
+namespace ZeroFive.Forms.Images.iOS
+{
+    /// <summary>
+    /// ImageCircle Implementation
+    /// </summary>
+    [Preserve]
+    public class RoundedImageRenderer : ImageRenderer
+    {
+        /// <summary>
+        /// Used for registration with dependency service
+        /// </summary>
+        public static void Init()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Element != null)
+            {
+                UpdateBorder();
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (e.PropertyName == VisualElement.HeightProperty.PropertyName ||
+                e.PropertyName == VisualElement.WidthProperty.PropertyName ||
+                e.PropertyName == RoundedImage.BorderRadiusProperty.PropertyName ||
+                e.PropertyName == RoundedImage.BorderColorProperty.PropertyName ||
+                e.PropertyName == RoundedImage.BorderThicknessProperty.PropertyName)
+            {
+                UpdateBorder();
+            }
+        }
+
+        private void UpdateBorder()
+        {
+            try
+            {
+                var radius = (Element as RoundedImage).BorderRadius;
+
+                if (radius < 0)
+                {
+                    radius = Math.Min(Element.Width, Element.Height) / 2.0;
+                }
+
+                Control.Layer.CornerRadius = (float)radius;
+                Control.Layer.MasksToBounds = false;
+                Control.Layer.BorderColor = ((RoundedImage)Element).BorderColor.ToCGColor();
+                Control.Layer.BorderWidth = ((RoundedImage)Element).BorderThickness;
+                Control.ClipsToBounds = true;
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine("Unable to create circle image: " + ex);
+            }
+        }
+    }
+}
---
+++ ZeroFive.Forms.Images/RoundedImage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ZeroFive.Forms.Images
+{
+    /// <summary>
+    /// Rounded image.
+    /// </summary>
+    public class RoundedImage : Image
+    {
+        /// <summary>
+        /// Thickness property of border
+        /// </summary>
+        public static readonly BindableProperty BorderThicknessProperty =
+            BindableProperty.Create<RoundedImage, int>(
+                p => p.BorderThickness, 0);
+
+        /// <summary>
+        /// Border thickness of image
+        /// </summary>
+        public int BorderThickness
+        {
+            get { return (int)GetValue(BorderThicknessProperty); }
+            set { SetValue(BorderThicknessProperty, value); }
+        }
+
+        /// <summary>
+        /// Color property of border
+        /// </summary>
+        public static readonly BindableProperty BorderColorProperty =
+            BindableProperty.Create<RoundedImage, Color>(
+                p => p.BorderColor, Color.White);
+
+        /// <summary>
+        /// Border Color of image
+        /// </summary>
+        public Color BorderColor
+        {
+            get { return (Color)GetValue(BorderColorProperty); }
+            set { SetValue(BorderColorProperty, value); }
+        }
+
+        /// <summary>
+        /// The border radius property.
+        /// </summary>
+        public static readonly BindableProperty BorderRadiusProperty =
+            BindableProperty.Create<RoundedImage, double>(
+                p => p.BorderRadius, -1.0);
+
+        /// <summary>
+        /// Border radius of image.
+        /// </summary>
+        public double BorderRadius
+        {
+            get { return (double)GetValue(BorderRadiusProperty); }
+            set { SetValue(BorderRadiusProperty, value); }
+        }
+    }
+}
+
